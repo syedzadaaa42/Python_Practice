87ff021094ceb97a2790ad07f86597b6bd1c171f
Asad <syedzadaaa42@gmail.com>
2024-06-08 14:21:45 +0100

    *

---
+++ Ch#1_PS.py
@@ -0,0 +1,23 @@
+import http.client 
+import json 
+
+connection = http.client.HTTPSConnection("www.google.com")
+
+connection.request("GET", "/todos/1")
+
+response = connection.getresponse()
+
+print("status:", response.status)
+print("Reason:", response.reason)
+
+data = response.read()
+
+#Try decoding as a json:
+
+try:
+    json_data = json.loads(data)
+    pretty_json = json.dumps(json_data, indent=4)
+    print("Content", pretty_json)
+except json.JSONDecodeError:
+    print("Content", data.decode('utf-8'))
+    
