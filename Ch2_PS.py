00d7a864b5d8813b7002854850e96f408b04a2ce
Asad <syedzadaaa42@gmail.com>
2024-06-13 14:36:27 +0100

    Doing new exercies

---
+++ Ch2_PS.py
@@ -0,0 +1,21 @@
+a = 6
+b = 66
+c = 66.666666
+d = "asad"
+e = True
+
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+
+#Exercise:1 Basic Varibales and Operation
+print("Exercise 1: ==========================")
+a = 30
+b = "Asad"
+c = 30.33
+print(f"a = {a}, type: {type(a)}")
+print(f"b = {b}, type: {type(b)}")
+print(f"c = {c}, type: {type(c)}")
